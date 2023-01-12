60c75efff934fe3d9101e788f2037f85036723e1
Valentin Vila <vilavalentin@gmail.com>
2023-01-12 01:24:54 +0100

    finished responsive mobile

--- components/Hero.js
+++ components/Hero.js
@@ -5,8 +5,8 @@ const Hero = () => {
     <div className="container mx-auto bottom-0 flex">
       <div className="font-montserrat grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 place-items-center ml-10 ">
         <div className="flex mx-auto text-black flex-col">
-          <h1 className="text-2xl md:text-4xl font-black mb-2 md:mb-14">CREANDO ESTADÍSTICAS DE LOS PACIENTES</h1>
-          <h4 className="text-sm md:text-xl mb-2 md:mb-14 font-regular">Somos la primer plataforma que crea estadísticas de los pacientes y los sincroniza con los hospitales.</h4>
+          <h1 className="text-4xl font-black mb-10 sm:mb-14">CREANDO ESTADÍSTICAS DE LOS PACIENTES</h1>
+          <h4 className="text-xl mb-10 sm:mb-14 font-regular">Somos la primer plataforma que crea estadísticas de los pacientes y los sincroniza con los hospitales.</h4>
           <a target="_blank" rel="noopener noreferrer" href="https://form.jotform.com/230076352214648">
             <button className="bg-[#96C3D6] w-52 h-12 rounded-full text-white font-black">BOOK A DEMO</button>
           </a>
@@ -19,7 +19,7 @@ const Hero = () => {
             <h4 className="text-xl mb-14 font-regular">
               Eve revoluciona la<br></br> forma en que los<br></br> hospitales obtienen y<br></br> comparten datos.
             </h4>
-            <div className="w-60 h-60 relative flex justify-center items-center ml-0">
+            <div className="w-60 h-60 relative flex items-center">
               <div className="bg-white w-5/6 h-5/6 absolute flex items-center flex-col justify-evenly">
                 <div>
                   <p className="font-bold text-[#96C3D6] text-2xl">1000+</p>
@@ -30,7 +30,7 @@ const Hero = () => {
                   <p>Hospitales</p>
                 </div>
               </div>
-              <div className="bg-[#90AFDF] w-5/6 h-5/6 ml-10 mb-10"></div>
+              <div className="bg-[#90AFDF] w-5/6 h-5/6 ml-5 mb-8"></div>
             </div>
           </div>
         </div>
--- components/Info.js
+++ components/Info.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div className="bg-[#C0DAFF]">
+    <div className="bg-[#C0DAFF] self-center">
       <div className="container mx-auto my-24">
         <div className="grid grid-cols-2 text-center ml-10">
           <div className="col-span-4">
-            <h1 className="text-2xl md:text-4xl font-bold mb-24 md:mb-24">
+            <h1 className="text-4xl font-bold mb-24 md:mb-24">
               Si tienes dudas,<br></br>pregúntale a tus datos
             </h1>
           </div>
           <div className="text-left">
             <div className="mb-10">
-              <h1 className="text-md lg:text-3xl font-bold mb-2 md:mb-14 lg:mb-14">Búsqueda a tu manera</h1>
-              <h4 className="text-xs lg:text-xl font-regular mb-2">
-                Eve te permite realizar y combinar muchos tipos de búsquedas: <br></br>por hospital, por pacientes, por genero, por enfermedad, por grupo<br></br> etario, de la
-                forma que desees. Comienza por lo más simple con<br></br> una pregunta y observa a dónde te lleva.
+              <h1 className="text-3xl font-bold mb-2 sm:mb-14">Búsqueda a tu manera</h1>
+              <h4 className="text-xl font-regular mb-2">
+                Eve te permite realizar y combinar muchos tipos de búsquedas: por hospital, por pacientes, por genero, por enfermedad, por grupo etario, de la forma que desees.
+                Comienza por lo más simple con una pregunta y observa a dónde te lleva.
               </h4>
             </div>
             <div>
-              <h1 className="text-md lg:text-3xl font-bold mb-2 md:mb-14 lg:mb-14">Analiza a escala</h1>
-              <h4 className="text-xs lg:text-xl font-regular mb-2">
-                Es facil encontrar pacientes que coincidan con tu busqueda.<br></br> Pero, ¿cómo le damos sentido a 100 mil pacientes y a un millón?<br></br> Eve te permite obtener
-                una vista mas general para<br></br> explorar tendencias y patrones en tus datos.
+              <h1 className="text-3xl font-bold mb-2 sm:mb-14">Analiza a escala</h1>
+              <h4 className="text-xl font-regular mb-2">
+                Es facil encontrar pacientes que coincidan con tu busqueda. Pero, ¿cómo le damos sentido a 100 mil pacientes y a un millón? Eve te permite obtener una vista mas
+                general para explorar tendencias y patrones en tus datos.
               </h4>
             </div>
           </div>
